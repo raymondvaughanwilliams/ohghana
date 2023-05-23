123f637c1d55284ef49c0d303388cacfd0ac648b
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2023-05-23 16:05:04 +0000

    Begin datatable integration

--- structure/static/js/farmers/datatable.js
+++ structure/static/js/farmers/datatable.js
@@ -5,32 +5,40 @@
 
 
 let farmersTable = $("#farmers-table-dt").DataTable({
-    dom: 'Bfltirp',
-    // fixedHeader: true,
-    // fixedColumns: {
-    //     left: 2,
-    // },
-    autoWidth: false,
-    // orderCellsTop: true,
-    deferRender: true,
-    scrollY: 500,
-    scroller: true,
-    // select: true,
+    dom: 'lBftirp',
+    buttons: [
+        'pdf',
+        'csv',
+        {
+            extend: 'selected',
+            text: 'Delete',
+            action: function (e, table, button, config) {
+                let selectedRows = table.rows({selected: true}).indexes();
+                Array.from(selectedRows).forEach((value, index) => {
+                    console.log(table.row(value).data());
+                    table.row(value).remove().draw();
+                })
+            }
+        }
+    ],
+
+    columnDefs: [{
+        orderable: false,
+        className: 'select-checkbox',
+        targets: 0
+    }],
+    select: {
+        style: 'os',
+        selector: 'td:first-child'
+    },
     ajax: {url: "/farmersapi"},
-    // responsive: {
-    //     breakpoints: [
-    //         {name: 'bigdesktop', width: Infinity},
-    //         {name: 'meddesktop', width: 1480},
-    //         {name: 'smalldesktop', width: 1280},
-    //         {name: 'medium', width: 1188},
-    //         {name: 'tabletl', width: 1024},
-    //         {name: 'btwtabllandp', width: 848},
-    //         {name: 'tabletp', width: 768},
-    //         {name: 'mobilel', width: 480},
-    //         {name: 'mobilep', width: 320}
-    //     ]
-    // },
     columns: [
+        {
+            data: null,
+            render: function render(data, type, row, meta) {
+                return '';
+            }
+        },
         {
             data: 'cooperative',
         },
@@ -62,56 +70,6 @@ let farmersTable = $("#farmers-table-dt").DataTable({
             },
         }
     ],
-    initComplete: function () {
-        var api = this.api();
-
-        // For each column
-        api
-            .columns()
-            .eq(0)
-            .each(function (colIdx) {
-                // Set the header cell to contain the input element
-                var cell = $('.filters th').eq(
-                    $(api.column(colIdx).header()).index()
-                );
-                var title = $(cell).text();
-                $(cell).html('<input type="text" placeholder="' + title + '" />');
-
-                // On every keypress in this input
-                $(
-                    'input',
-                    $('.filters th').eq($(api.column(colIdx).header()).index())
-                )
-                    .off('keyup change')
-                    .on('change', function (e) {
-
-                        // Get the search value
-                        $(this).attr('title', $(this).val());
-                        var regexr = '({search})'; //$(this).parents('th').find('select').val();
-
-                        var cursorPosition = this.selectionStart;
-                        // Search the column for that value
-                        api
-                            .column(colIdx)
-                            .search(
-                                this.value != ''
-                                    ? regexr.replace('{search}', '(((' + this.value + ')))')
-                                    : '',
-                                this.value != '',
-                                this.value == ''
-                            )
-                            .draw();
-                    })
-                    .on('keyup', function (e) {
-                        e.stopPropagation();
-
-                        $(this).trigger('change');
-                        $(this)
-                            .focus()[0]
-                            .setSelectionRange(cursorPosition, cursorPosition);
-                    });
-            });
-    },
 });
 
 
